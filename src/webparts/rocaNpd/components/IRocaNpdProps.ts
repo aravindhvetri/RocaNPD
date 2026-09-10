a8c2d2afb80a5f0bb68ec919cd1afca6c3b9b09f
aravindhvetri <2002aravinthv@gmail.com>
2026-09-10 12:15:36 +0530

    First initial Datas push to main 12:15PM 10/09/2026

---
+++ src/webparts/rocaNpd/components/IRocaNpdProps.ts
@@ -0,0 +1,6 @@
+export interface IRocaNpdProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  userDisplayName: string;
+}
---
+++ src/webparts/rocaNpd/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IRocaNpdWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'RocaNpdWebPartStrings' {
+  const strings: IRocaNpdWebPartStrings;
+  export = strings;
+}
